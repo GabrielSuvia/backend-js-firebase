6fee55cd70453e24171a59a2a95154694cf14a20
gabriel <ggabrielomar123@hotmail.com>
2025-02-07 20:35:34 -0400

    firebase

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 const admin = require('firebase-admin');
-const keyDb= require('./serviceAccountKey.json')
+const keyDb= require('./serviceAccountKey')
+
 admin.initializeApp({
   credential: admin.credential.cert(keyDb),
  // databaseURL: 'https://tu-proyecto.firebaseio.com'
