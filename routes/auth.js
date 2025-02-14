1b1b15c07c9891f06a7cb6cd56e11ce63521158e
gabriel <ggabrielomar123@hotmail.com>
2025-02-14 18:06:57 -0400

    Implementation of JWT in route

--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const admin = require('../firebase');
 const User = require('../models/User')
+const admin = require('../firebase');
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
+//const credentialKey = process.env.CREDENTIAL_FIREBASE
+
+const createToken = async (id,rol="visitor")=>{
+const payload = {
+  sub:id,
+  rol
+}
+const token = jwt.sign(payload,process.env.SECRET_KEY);
+return token;
+}
+
 
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
-
+  
   try {
-    const listUser = await User.getAllUsers(admin);
-    console.log("ARRAY", listUser)
-    const userFound = listUser.filter((ele)=> ele.password === password && ele.email === email)
-    console.log(typeof userFound)
-    if (!userFound) {
-      console.log("No se encontraron documentos");
-    } else {
-      console.log("Se encontraron documentos", userFound);
-    }
-    console.log("todo bien")
-    res.status(200).json({ message: 'Login exitoso' });
-    
-  } catch (error) {;
-    res.status(401).json({ error: 'Credenciales no validas' });
+    const userList = await User.getAllUsers(admin);
+    const userFound = userList.filter((user)=> user.email === email && user.password === password);
+
+    if(userFound.length === 1){
+    const token = await createToken(userFound[0].id,"user");
+    return res.status(200).json({ message: token });
+    };
+
+   return res.status(401).json({ error: 'Credenciales no validas' });
+
+  } catch (error) {
+   return res.status(401).json({ error: 'Credenciales no validas' });
   }
 });
 
